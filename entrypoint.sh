b5f3fc27a898afb7bf7bd87010ed92433ff2d048
Vlad Ionescu <Vlaaaaaaad@users.noreply.github.com>
2019-09-26 20:37:30 +0300

    s/arg/flags

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-tflint --format=checkstyle ${INPUT_ARGS} \
+tflint --format=checkstyle ${INPUT_FLAGS} \
   | reviewdog -f=checkstyle -name="tflint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
