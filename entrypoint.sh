e483151f0b5ccf76d8ed027d8af269b6be449230
Vlad Ionescu <Vlaaaaaaad@users.noreply.github.com>
2020-02-09 13:24:42 +0000

    One-line

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,7 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 tflint --format=checkstyle ${INPUT_FLAGS} "${INPUT_WORKING_DIRECTORY}" \
   | reviewdog -f=checkstyle -name="tflint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
 
-exit_code=$?
-tflint_return="${PIPESTATUS[0]}"
-reviewdog_return="${PIPESTATUS[1]}"
+tflint_return="${PIPESTATUS[0]}" reviewdog_return="${PIPESTATUS[1]}" exit_code=$?
 
 echo ::set-output name=tflint-return-code::"${tflint_return}"
 echo ::set-output name=reviewdog-return-code::"${reviewdog_return}"
