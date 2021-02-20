6216cf9663c9a234ebb74ebd7bac83246bd33d1f
Vlad Ionescu <Vlaaaaaaad@users.noreply.github.com>
2021-02-20 17:37:26 +0200

    Customisable tflint version (#34)
    * Customizable tflint version
    
    * Use a custom tflint version in a test
    
    * Update action.yml
    
    Co-authored-by: Josh Soref <jsoref@users.noreply.github.com>
    
    * Pipe curl directly to bash
    
    Co-authored-by: haya14busa <haya14busa@gmail.com>
    
    Co-authored-by: Josh Soref <jsoref@users.noreply.github.com>
    Co-authored-by: haya14busa <haya14busa@gmail.com>

--- script.sh
+++ script.sh
@@ -10,7 +10,7 @@ curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.s
 echo '::endgroup::'
 
 echo '::group:: Installing tflint ... https://github.com/terraform-linters/tflint'
-curl -L "$(curl -Ls https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" -o tflint.zip && unzip tflint.zip -d "${TEMP_PATH}" && rm tflint.zip
+curl -sfL https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | TFLINT_VERSION="${INPUT_TFLINT_VERSION}" bash
 echo '::endgroup::'
 
 
