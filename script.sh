e94610f3ded91dde30cf72d23bf2591b683763fa
Sebastian Olsson <sebastian.olsson@algodx.com>
2023-04-28 03:51:08 +0200

    fix: Pass GITHUB_TOKEN to `tflint --init` (#64)
    Co-authored-by: ICHINOSE Shogo <shogo82148@gmail.com>

--- script.sh
+++ script.sh
@@ -73,7 +73,7 @@ done
 case "${INPUT_TFLINT_INIT:-false}" in
     true)
         echo "::group:: Initialize tflint from local configuration"
-        TFLINT_PLUGIN_DIR="${TFLINT_PLUGIN_DIR}" "${TFLINT_PATH}/tflint" --init
+        TFLINT_PLUGIN_DIR="${TFLINT_PLUGIN_DIR}" GITHUB_TOKEN="${INPUT_GITHUB_TOKEN}" "${TFLINT_PATH}/tflint" --init
         echo "::endgroup::"
         ;;
     false)
